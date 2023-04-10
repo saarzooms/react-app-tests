a6672e488edc4effe0abd1fcc3357c1f477d2771
arzoo.shaikh <arzoo.rkcet@gmail.com>
2023-04-10 14:39:04 +0530

    find year from dob
    without button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  
+  const [age, setAge] = useState(0);
 
+  function calcAge(dob:string){
+    const birthDate = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear()
+    setAge(age);
+  }
   return (
-  <div>
-      <input type="text" name="mydata" onChange={(e)=>{console.log(e.target.value);
-      }}/> 
+  <><div>
+       <input type="date" onChange={(e)=>{
+        calcAge(e.target.value);
+       }}/><br/><p>You are {age} years old</p>
   </div>
+  </>
   );
 }
 
